0a00b2782a34463c2cd906089f75e9086cb73dcf
Hyounoh <hyounohshim@gmail.com>
2020-06-18 18:22:08 +0900

    [ENV] Set java version to 1.8 and add permission for using internet

--- android/app/src/main/java/com/mug8/beagoodgolfer/MainActivity.kt
+++ android/app/src/main/java/com/mug8/beagoodgolfer/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.mug8.beagoodgolfer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-
+import androidx.appcompat.app.AppCompatActivity
 import com.mug8.beagoodgolfer.constant.TAG
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
